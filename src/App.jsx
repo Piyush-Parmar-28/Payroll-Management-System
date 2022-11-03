ae8a8b487a9a7a5d00378f3abb7b5e9557e48fc5
Piyush Parmar <2822piyushparmar@gmail.com>
2022-11-03 20:48:37 +0530

    Initialised Project & Added Navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+const App = () => {
+    return (
+        <Fragment>
+            <BrowserRouter>
+                <Routes>
+                    
+                </Routes>
+            </BrowserRouter>
+        </Fragment>
+    )
+}
+
+export default App
